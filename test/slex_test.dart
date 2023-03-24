985083716ef99d830db78e1135425f98c320fc6f
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-24 15:44:02 +0100

    Update slex_test.dart

--- test/slex_test.dart
+++ test/slex_test.dart
@@ -2,11 +2,22 @@
 /// (c) 2023 by Andreas Schwenk <mailto:contact@compiler-construction.com>
 /// License: GPL-3.0-or-later
 
-//import 'package:slex/slex.dart';
+import 'package:slex/slex.dart';
 //import 'package:test/test.dart';
 
 void main() {
   // TODO: migrate code from https://github.com/multila/multila-lexer/blob/main/test/lex_TESTS.dart
+
+  var src = '''
+Hello,
+  world!
+''';
+  var lex = Lexer();
+  lex.pushSource('', src);
+  while (lex.isNotEnd()) {
+    print(lex.getToken());
+    lex.next();
+  }
 
   /*group('A group of tests', () {
     final awesome = Awesome();
